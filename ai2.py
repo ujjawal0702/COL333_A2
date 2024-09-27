88daa44d054315796cf8c5d31178b3b68d40b509
ujjawal0702 <157407378+ujjawal0702@users.noreply.github.com>
2024-09-27 14:31:34 +0530

    Add files via upload

---
+++ ai2.py
@@ -0,0 +1,45 @@
+import time
+import math
+import random
+import numpy as np
+from helper import *
+
+
+class AIPlayer:
+
+    def __init__(self, player_number: int, timer):
+        """
+        Intitialize the AIPlayer Agent
+
+        # Parameters
+        `player_number (int)`: Current player number, num==1 starts the game
+        
+        `timer: Timer`
+            - a Timer object that can be used to fetch the remaining time for any player
+            - Run `fetch_remaining_time(timer, player_number)` to fetch remaining time of a player
+        """
+        self.player_number = player_number
+        self.type = 'ai'
+        self.player_string = 'Player {}: ai'.format(player_number)
+        self.timer = timer
+
+    def get_move(self, state: np.array) -> Tuple[int, int]:
+        """
+        Given the current state of the board, return the next move
+
+        # Parameters
+        `state: Tuple[np.array]`
+            - a numpy array containing the state of the board using the following encoding:
+            - the board maintains its same two dimensions
+            - spaces that are unoccupied are marked as 0
+            - spaces that are blocked are marked as 3
+            - spaces that are occupied by player 1 have a 1 in them
+            - spaces that are occupied by player 2 have a 2 in them
+
+        # Returns
+        Tuple[int, int]: action (coordinates of a board cell)
+        """
+
+        # Do the rest of your implementation here
+        raise NotImplementedError('Whoops I don\'t know what to do')
+
